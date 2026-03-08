304c257a0418f4a24103431be2195c5627909f75
JJ-223 <151877132+JJ-223@users.noreply.github.com>
2026-03-08 00:29:46 -0800

    Enable dynamic question loading from files
    Refactor Trivia game to allow dynamic question loading from files for different categories.

--- Trivia.java
+++ Trivia.java
@@ -4,6 +4,9 @@ import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -11,6 +14,7 @@ public class Trivia extends JFrame{
     private String username;
     private JPanel cardPanel, welcomePanel, usernamePanel, gamePanel, resultPanel;
     private JButton startButton, nextButton, usernameButton, playButton, playAgainButton;
+    private JButton japanButton, mexicoButton, australiaButton, taiwanButton, greeceButton, cambodiaButton;
     private JTextField inputField;
     private CardLayout cardLayout;
     private JLabel questionLabel, resultLabel;
@@ -21,7 +25,7 @@ public class Trivia extends JFrame{
     public Trivia(){
         currentQuestionIdx = 0;
         currentScore = 0;
-        importQuestions();
+        //importQuestions();
 
         cardLayout = new CardLayout();
         cardPanel = new JPanel(cardLayout);
@@ -39,10 +43,16 @@ public class Trivia extends JFrame{
         ButtonHandler handler = new ButtonHandler();
         startButton.addActionListener(handler);
         usernameButton.addActionListener(handler);
-        playButton.addActionListener(handler);
+        //playButton.addActionListener(handler);
         nextButton.addActionListener(handler);
         playAgainButton.addActionListener(handler);
 
+        japanButton.addActionListener(handler);
+        mexicoButton.addActionListener(handler);
+        australiaButton.addActionListener(handler);
+        taiwanButton.addActionListener(handler);
+        greeceButton.addActionListener(handler);
+        cambodiaButton.addActionListener(handler);
         //add all panels to cardPanel
         add(cardPanel);
     }
@@ -53,12 +63,16 @@ public class Trivia extends JFrame{
             if(event.getSource() == startButton){
                 cardLayout.show(cardPanel, "U");
             }
-            else if (event.getSource() == playButton){
+            /*else if (event.getSource() == playButton){
+                currentQuestionIdx = 0;
+                currentScore = 0;
+                importQuestions("Japan.txt");
                 loadNextQuestions();
                 cardLayout.show(cardPanel, "G");
-            }
+            }*/
             else if(event.getSource() == nextButton){
                 //createResultPanel();
+                resultLabel.setText("Congrats " + username + " on earning " + currentScore + " pts!");
                 cardLayout.show(cardPanel, "R");
             }
             else if (event.getSource() == usernameButton){
@@ -70,6 +84,48 @@ public class Trivia extends JFrame{
                 currentScore = 0;
                 loadNextQuestions();
                 cardLayout.show(cardPanel, "W");
+            }
+            else if (event.getSource() == japanButton){
+                currentQuestionIdx = 0;
+                currentScore = 0;
+                importQuestions("src/Japan.txt");
+                loadNextQuestions();
+                cardLayout.show(cardPanel, "G");
+            }
+            else if (event.getSource() == mexicoButton){
+                currentQuestionIdx = 0;
+                currentScore = 0;
+                importQuestions("src/Mexico.txt");
+                loadNextQuestions();
+                cardLayout.show(cardPanel, "G");
+            }
+            else if (event.getSource() == australiaButton){
+                currentQuestionIdx = 0;
+                currentScore = 0;
+                importQuestions("src/Australia.txt");
+                loadNextQuestions();
+                cardLayout.show(cardPanel, "G");
+            }
+            else if (event.getSource() == taiwanButton){
+                currentQuestionIdx = 0;
+                currentScore = 0;
+                importQuestions("src/Taiwan.txt");
+                loadNextQuestions();
+                cardLayout.show(cardPanel, "G");
+            }
+            else if (event.getSource() == greeceButton){
+                currentQuestionIdx = 0;
+                currentScore = 0;
+                importQuestions("src/Greece.txt");
+                loadNextQuestions();
+                cardLayout.show(cardPanel, "G");
+            }
+            else if (event.getSource() == cambodiaButton){
+                currentQuestionIdx = 0;
+                currentScore = 0;
+                importQuestions("src/Cambodia.txt");
+                loadNextQuestions();
+                cardLayout.show(cardPanel, "G");
             }
         }
     }
@@ -108,39 +164,56 @@ public class Trivia extends JFrame{
     private void createUsernamePanel(){
         usernamePanel = new JPanel(new BorderLayout());
         inputField = new JTextField("Enter a username: ");
-        inputField.setFont(new Font("Arial", Font.PLAIN, 50));
+        inputField.setFont(new Font("Arial", Font.PLAIN, 40));
 
         usernameButton = new JButton("Save the name");
-        usernameButton.setFont(new Font("Arial", Font.BOLD, 35));
+        usernameButton.setFont(new Font("Arial", Font.BOLD, 30));
 
         JPanel usernameButtonPanel = new JPanel(new FlowLayout());
         usernameButtonPanel.add(inputField);
         usernameButtonPanel.add(usernameButton);
         usernamePanel.add(usernameButtonPanel, BorderLayout.NORTH);
 
-        JPanel buttonPanel = new JPanel(new FlowLayout());
+        JPanel categoryPanel = new JPanel(new GridLayout(2,3,10, 10));
+        japanButton = new JButton("Japan");                 japanButton.setFont(new Font("Arial", Font.BOLD, 35));
+        mexicoButton = new JButton("Mexico");               mexicoButton.setFont(new Font("Arial", Font.BOLD, 35));
+        australiaButton = new JButton("Australia");         australiaButton.setFont(new Font("Arial", Font.BOLD, 35));
+        taiwanButton = new JButton("Taiwan");               taiwanButton.setFont(new Font("Arial", Font.BOLD, 35));
+        greeceButton = new JButton("Greece");               greeceButton.setFont(new Font("Arial", Font.BOLD, 35));
+        cambodiaButton = new JButton("Cambodia");           cambodiaButton.setFont(new Font("Arial", Font.BOLD, 35));
+        categoryPanel.add(japanButton);
+        categoryPanel.add(mexicoButton);
+        categoryPanel.add(australiaButton);
+        categoryPanel.add(taiwanButton);
+        categoryPanel.add(greeceButton);
+        categoryPanel.add(cambodiaButton);
+
+        usernamePanel.add(categoryPanel, BorderLayout.CENTER);
+
+        /*JPanel buttonPanel = new JPanel(new FlowLayout());
         playButton = new JButton("Play");
         playButton.setFont(new Font("Arial", Font.BOLD, 35));
         buttonPanel.add(playButton);
-        usernamePanel.add(buttonPanel, BorderLayout.SOUTH);
+        usernamePanel.add(buttonPanel, BorderLayout.SOUTH);*/
         cardPanel.add(usernamePanel, "U");
     }
     private void createGamePanel(){
         gamePanel = new JPanel(new BorderLayout());
         //gamePanel.add(new JLabel("Quiz Screen"));
         questionLabel = new JLabel("Question #1", SwingConstants.CENTER);
-        questionLabel.setFont(new Font("Arial", Font.BOLD, 32));
+        questionLabel.setFont(new Font("Arial", Font.BOLD, 35));
         gamePanel.add(questionLabel, BorderLayout.NORTH);
         //JLabel gameLabel = new JLabel("Quiz Screen");
         //gameLabel.setFont(new Font("Arial", Font.BOLD, 50));
         //gamePanel.add(gameLabel);
         nextButton = new JButton("Go to results");
-        nextButton.setFont(new Font("Arial", Font.BOLD, 35));
+        nextButton.setFont(new Font("Arial", Font.BOLD, 30));
         gamePanel.add(nextButton, BorderLayout.SOUTH);
 
         JPanel buttonPanel = new JPanel(new GridLayout(2, 2, 10, 10));
         for (int i = 0; i < 4; i++){
             optionButtons[i] = new JButton("Option " + (i+1));
+            optionButtons[i].setFont(new Font("Arial", Font.BOLD, 30));
             OptionButtonsHandler optionButtonsHandler = new OptionButtonsHandler(i);
             optionButtons[i].addActionListener(optionButtonsHandler);
             buttonPanel.add(optionButtons[i]);
@@ -156,6 +229,8 @@ public class Trivia extends JFrame{
         resultLabel = new JLabel();
         resultLabel.setFont(new Font("Arial", Font.BOLD, 50));
         //resultLabel.setText("Congrats " + username + " on earning " + currentScore + " pts!");
+        resultLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        resultLabel.setVerticalAlignment(SwingConstants.CENTER);
         resultPanel.add(resultLabel, BorderLayout.CENTER);
 
         playAgainButton = new JButton("Play again?");
@@ -164,40 +239,28 @@ public class Trivia extends JFrame{
         cardPanel.add(resultPanel, "R");
     }
 
-    private void importQuestions(){
+    private void importQuestions(String filename){
         questionPair = new ArrayList<>();
         correctAnswers = new ArrayList<>();
 
-        questionPair.add(List.of("Question 1: What do we refer to BST in 4319?",
-                "British Summer Time", "Breadth Search Tree", "Binary Search Tree", "None of the above"));
-        correctAnswers.add(2);
-        questionPair.add(List.of("Question 2: Which class belongs to Java Swing?",
-                "NumberFormatException", "String", "Graphics", "None of the above"));
-        correctAnswers.add(3);
-        questionPair.add(List.of("Question 3: What is the capital of France?",
-                "Paris", "London", "Berlin", "Rome"));
-        correctAnswers.add(0);
-        questionPair.add(List.of("Question 4: Which planet is known as the Red Planet?",
-                "Earth", "Venus", "Mars", "Jupiter"));
-        correctAnswers.add(2);
-        questionPair.add(List.of("Question 5: Recursion always needs a?",
-                "Loop", "Base Case", "Queue", "Stack"));
-        correctAnswers.add(1);
-        questionPair.add(List.of("Question 1: What do we refer to BST in 4319?",
-                "British Summer Time", "Breadth Search Tree", "Binary Search Tree", "None of the above"));
-        correctAnswers.add(2);
-        questionPair.add(List.of("Question 2: Which class belongs to Java Swing?",
-                "NumberFormatException", "String", "Graphics", "None of the above"));
-        correctAnswers.add(3);
-        questionPair.add(List.of("Question 3: What is the capital of France?",
-                "Paris", "London", "Berlin", "Rome"));
-        correctAnswers.add(0);
-        questionPair.add(List.of("Question 4: Which planet is known as the Red Planet?",
-                "Earth", "Venus", "Mars", "Jupiter"));
-        correctAnswers.add(2);
-        questionPair.add(List.of("Question 5: Recursion always needs a?",
-                "Loop", "Base Case", "Queue", "Stack"));
-        correctAnswers.add(1);
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            String line;
+
+            while((line = reader.readLine()) != null){
+                String[] parts = line.split("\\|");
+
+                List<String> question  = new ArrayList<>();
+                for(int i = 0; i < 5; i++){
+                    question.add(parts[i]);
+                }
+                questionPair.add(question);
+                correctAnswers.add(Integer.parseInt(parts[5]));
+            }
+            reader.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private void loadNextQuestions(){
@@ -213,3 +276,4 @@ public class Trivia extends JFrame{
         }
     }
 }
+
